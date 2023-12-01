8bc350cc5303fcde4bb11c4b314842b32883742d
KubaJ24 <j.jarosz@onet.pl>
2023-12-01 14:08:13 +0100

    Wysyłanie przez UART

--- Sterownik klawiatury 4x4/ATm_keypad/ATm_keypad.ino
+++ Sterownik klawiatury 4x4/ATm_keypad/ATm_keypad.ino
@@ -7,53 +7,22 @@ Wiersze - PC0 do PC3
 Wiersze PULL-DOWN 1k
 */
 
-uint8_t var = 0;
+int a = 0;
 
 void PIN_SETUP(void);
+void UART_SETUP(void);
+void GET_KEY(void);
+void DISPLAY_KEY(uint8_t num);
 
 void setup() {
 
-  Serial.begin(9600);
-
   PIN_SETUP();
-  PORTC |= 0b0000;
+  UART_SETUP();
+
 }
 
 void loop() {
-  for(uint8_t i = 0; i < 4; i++){
-    PORTC = (1 << i);
-    if(PINB == 1){ 
-      Serial.print("PINB: ");
-      Serial.print(PINB, BIN);
-      Serial.print("   i: ");
-      Serial.println(i); 
-      delay(250); 
-    }
-    else if(PINB == 2){ 
-      Serial.print("PINB: ");
-      Serial.print(PINB);
-      Serial.print("   i: ");
-      Serial.println(i + 5); 
-      while(PINB == 2){}
-      delay(20); 
-    }
-    else if(PINB == 4){ 
-      Serial.print("PINB: ");
-      Serial.print(PINB);
-      Serial.print("   i: ");
-      Serial.println(i + 9); 
-      while(PINB == 4){}
-      delay(20); 
-    }
-    else if(PINB == 8){ 
-      Serial.print("PINB: ");
-      Serial.print(PINB);
-      Serial.print("   i: ");
-      Serial.println(i + 13); 
-      while(PINB == 8){}
-      delay(20); 
-    }
-  }
+  GET_KEY();
 }
 
 void PIN_SETUP(void){
@@ -64,4 +33,43 @@ void PIN_SETUP(void){
   //ROWS
   DDRB = 0x00;
   PORTB = 0x00;
+}
+
+void UART_SETUP(void){
+  //UART SET, 8-BIT WORD LENGTH, 1-BIT STOP
+  UCSR0C = (1 << UCSZ00) | (1 << UCSZ01);
+  //BAUD RATE 9600
+  UBRR0L = 103;
+  //TRANSMITTER ENABLE
+  UCSR0B = (1 << TXEN0);
+}
+
+void GET_KEY(void){
+  for(uint8_t i = 0; i < 4; i++){
+    PORTC = (1 << i);
+    if(PINB == 1){ 
+      DISPLAY_KEY(i);
+      delay(250); 
+    }
+    else if(PINB == 2){ 
+      DISPLAY_KEY(i + 5);
+      while(PINB == 2){}
+      delay(20); 
+    }
+    else if(PINB == 4){ 
+      DISPLAY_KEY(i + 9);
+      while(PINB == 4){}
+      delay(20); 
+    }
+    else if(PINB == 8){ 
+      DISPLAY_KEY(i + 13);
+      while(PINB == 8){}
+      delay(20); 
+    }
+  } 
+}
+
+void DISPLAY_KEY(uint8_t num){
+  UDR0 = num;
+  while((UCSR0A & (1 << UDRE0)) == 0){ /* WAIT */ }
 }
