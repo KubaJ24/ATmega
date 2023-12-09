f7b1f6714fe1c4c72c3bc8326aa351c268df0d13
KubaJ24 <j.jarosz@onet.pl>
2023-12-09 12:14:47 +0100

    Zmiana na ATmega328p

--- Zasilacz/Zasilacz.ino
+++ Zasilacz/Zasilacz.ino
@@ -4,25 +4,58 @@
 //D8 -> PB4
 //D9 -> PB5 (ADC12)
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(A0, INPUT);
+const int ADC0 = 0;
+const int ADC1 = 1;
+const int ADC2 = 2;
 
-  ADCSRA = 0b11000111;
-  //Źródłem odniesienia jest AVcc (Vcc)
+uint16_t adc_result(uint8_t channel);
+
+float VOL_REG = 0;
+float VOL_5 = 0;
+float VOL_12 = 0;
+
+void setup() {
+
+  DDRC = 0x00;
+  PORTC = 0x00;
+  //A0 -> ADC0
   ADMUX = 0b01000000;
+  ADCSRA = 0b10000111;
+  
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-    // int loval = 0;
-    // int hival = 0;
-  int wynik = 0;
-    // loval = ADCL;
-    // hival = ADCH;
-    // wynik = (hival<<8) | loval; 
-  wynik = analogRead(A0);
-  Serial.println(wynik);
+  
+  VOL_REG = adc_result(ADC0);
+  VOL_REG = (VOL_REG / 1023.0) * 12.0;
+  Serial.println(VOL_REG);
   delay(500);
+
+  // VOL_5 = adc_result(ADC1);
+  // VOL_5 = (VOL_5 / 1023.0) * 5.0;
+  // Serial.println(VOL_5);
+  // delay(500);
+
+  // VOL_12 = adc_result(ADC2);
+  // VOL_12 = (VOL_12 / 1023.0) * 12.0;
+  // Serial.println(VOL_12);
+  // delay(500);
+
+}
+
+uint16_t adc_result(uint8_t channel){
+  ADMUX = 0b01000000;
+  ADMUX |= channel;
+  Serial.print("ADMUX:  ");
+  Serial.println(ADMUX, BIN);
+  uint8_t loval = 0;
+  uint8_t hival = 0;
+  uint16_t value = 0;
+  ADCSRA |= (1 << ADSC);
+  while((ADCSRA & (1 << ADSC)) == 1){  }
+  loval = ADCL;
+  hival = ADCH;
+  value = (hival<<8) | loval; 
+  return value;
 }
